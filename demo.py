8813c2ff929eaa273193b096e9dacadc0ce3afca
abdus-salam <abdussalam0419@gmail.com>
2024-02-24 02:04:04 +0600

    second part done

--- demo.py
+++ demo.py
@@ -7,8 +7,16 @@ soup = BeautifulSoup(web.content, "html.parser")
 
 # print(soup.prettify()) # make html tree 
 
-print(soup.title) # get title
+# print(soup.title) # get title
 
-print(soup.a) # get first anker tag
+# print(soup.a) # get first anker tag
 
-print(soup.p) # get first p tag
+# print(soup.p) # get first p tag
+
+p = soup.find("p") # finding first p from soup object
+print(p.string) # getting sting from p 
+print(type(p))
+
+p = soup.find_all("p") # finding first all p from soup object from the url
+print(len(p)) # getting len of p 
+print(type(p))
